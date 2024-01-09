e23af74109c59926249634698d811e1877676415
Jae Aeich <jh4official@gmail.com>
2024-01-09 17:10:34 +0530

    chore: add utils

---
+++ server/campus_hub/utils/db.py
@@ -0,0 +1,32 @@
+import os
+from pymongo import MongoClient
+
+
+class DBConnector:
+    def __init__(self) -> None:
+        """
+        Initializes the DBConnector object and establishes a connection to MongoDB using the provided environment variables.
+        """
+        self.username: str = os.environ.get("MONGO_USERNAME")
+        self.password: str = os.environ.get("MONGO_PASSWORD")
+        self.db_name: str = os.environ.get("MONGO_DB_NAME")
+        self.url: str = os.environ.get("MONGO_URL")
+
+        self.client: MongoClient = MongoClient(
+            self.url, username=self.username, password=self.password
+        )
+        self.db = self.client[self.db_name]
+
+    def ping(self) -> bool:
+        """
+        Checks the connection status to the MongoDB server.
+
+        Returns:
+            bool: True if the connection is successful, False otherwise.
+        """
+        try:
+            self.client.admin.command("ping")
+            return True
+        except Exception as e:
+            print(f"Error pinging MongoDB: {e}")
+            return False
