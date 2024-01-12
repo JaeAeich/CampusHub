814c4426a34eb3d839842f20ea11552f7e31a291
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2024-01-12 23:13:24 +0530

    fix: add service_id and offer_availability in models (#30)
    * fix: add service_id and availibitity of offers in models

--- server/campus_hub/models/offers.py
+++ server/campus_hub/models/offers.py
@@ -1,27 +1,30 @@
 from pydantic import BaseModel, validator
 from datetime import timedelta
-
+from typing import List
 
 class Offers(BaseModel):
     """
     Pydantic model representing a payment.
 
     Attributes:
-        product_id: product id of the product
+        product_ids: product ids having offer
+        service_id: service_id of the store
         store_id: store_id of the product
         discount: discount amount for the product
         validity_duration: expiration date of the discount
         offer_id: offer id for the product
     """
 
-    product_id: str
+    product_ids: List[str]
+    service_id: str
     store_id: str
     discount: float
     validity_duration: timedelta
     offer_id: str
 
     @validator(
-        "product_id",
+        "product_ids",
+        "service_id",
         "store_id",
         "discount",
         "validity_duration",
@@ -34,15 +37,17 @@ class Offers(BaseModel):
         Validator to ensure that required fields (product_id, store_id, discount, validity_duration, offer_id) are always present.
         """
         required_fields = [
-            "product_id",
+            "product_ids",
+            "service_id",
             "store_id",
             "discount",
             "validity_duration",
             "offer_id",
         ]
+        list_empty = len(value.get("product_ids")) < 1
         missing_fields = [field for field in required_fields if not value.get(field)]
 
-        if missing_fields:
+        if missing_fields or list_empty:
             raise ValueError(
                 f"The following fields are required and cannot be empty for payment: {', '.join(missing_fields)}"
             )
