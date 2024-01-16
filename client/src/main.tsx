58074c5d8042e836d09e9612c999a5beb86a68da
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2024-01-16 14:29:17 +0530

    frontend:feat: Google OAuth implemented

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID || ''}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </GoogleOAuthProvider>,
 );
