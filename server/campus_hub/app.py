4ecf3e5026de65d62ba5467f3fe3ccf82d1e7cf9
Jae Aeich <jh4official@gmail.com>
2024-01-19 20:41:49 +0530

    feat: add tests

--- server/campus_hub/app.py
+++ server/campus_hub/app.py
@@ -2,13 +2,19 @@ from pathlib import Path
 from connexion import FlaskApp
 from connexion.resolver import RelativeResolver
 
-app = FlaskApp(
-    __name__,
-    specification_dir="specs",
-    resolver=RelativeResolver("campus_hub.controllers"),
-)
-openapi_path = Path(__file__).parent / "specs" / "api.yaml"
-app.add_api(openapi_path)
+
+def create_app():
+    app = FlaskApp(
+        __name__,
+        specification_dir="specs",
+        resolver=RelativeResolver("campus_hub.controllers"),
+    )
+    openapi_path = Path(__file__).parent / "specs" / "api.yaml"
+    app.add_api(openapi_path)
+    return app
+
+
+app = create_app()
 
 if __name__ == "__main__":
     # for development only
