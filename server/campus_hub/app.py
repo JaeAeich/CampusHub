e61a04f0f88fde4653ed05fe2b76c06ded3a0c83
aishikanandi <aishikanandi@gmail.com>
2024-01-21 21:42:57 +0530

    fix: cors error

--- server/campus_hub/app.py
+++ server/campus_hub/app.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from connexion import FlaskApp
 from connexion.resolver import RelativeResolver
+from flask_cors import CORS
 
 app = FlaskApp(
     __name__,
@@ -9,7 +10,7 @@ app = FlaskApp(
 )
 openapi_path = Path(__file__).parent / "specs" / "api.yaml"
 app.add_api(openapi_path)
-
+CORS(app.app)
 if __name__ == "__main__":
     # for development only
     app.run(port=8081, debug=True)
