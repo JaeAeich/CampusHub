58c5fb6bc6b5a842f6dc353d8a881ff42157dd87
Jae Aeich <jh4official@gmail.com>
2024-01-31 20:32:46 +0530

    fix: NotFound comp css

--- client/src/components/NotFound.tsx
+++ client/src/components/NotFound.tsx
@@ -1,9 +1,8 @@
 function NotFound({ item }: { item: string }) {
   return (
     <div className="flex flex-col items-center justify-center m-10 mb-20">
-      {/* <Frown className="mx-auto" color='#D3D3D3' strokeWidth={1.5} size={150}/> */}
-      <img src="/noresult.gif" alt="noResults" className="sm:h-[180px] h-[90px]" />
-      <h2 className="align-center text-darkgray font-normal sm:text-lg text-base">
+      <img src="/noresult.gif" alt="noResults" className="h-16 w-42 md:h-48 md:w-92" />
+      <h2 className="align-center text-darkgray text-smm md:text-lg">
         {item} not found. Come back later!
       </h2>
     </div>
