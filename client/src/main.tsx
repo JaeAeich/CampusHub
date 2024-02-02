37d0f3b180555d14a7129d6b5663224b4bf80f1f
Jae Aeich <jh4official@gmail.com>
2024-02-02 18:02:06 +0530

    feat: add auth0

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import './index.css';
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientID = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain={domain}
+    clientId={clientID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </Auth0Provider>,
 );
