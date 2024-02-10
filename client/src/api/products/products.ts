589bd0f9f3a0a1a472a95326169b2f00a118a580
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2024-02-10 14:45:43 +0530

    fix: prettier fix

--- client/src/api/products/products.ts
+++ client/src/api/products/products.ts
@@ -1,9 +1,9 @@
-import { ErrorResponse } from "react-router-dom";
-import axios, { AxiosResponse } from "axios";
-import errorResponse from "@/utils/response";
-import { MessageResponse } from "../types";
-import baseURL from "../config";
-import Product from "./types";
+import { ErrorResponse } from 'react-router-dom';
+import axios, { AxiosResponse } from 'axios';
+import errorResponse from '@/utils/response';
+import { MessageResponse } from '../types';
+import baseURL from '../config';
+import Product from './types';
 
 /**
  * Represents the base URL for the services API.
@@ -12,35 +12,38 @@ const productsURL = `${baseURL}/products`;
 
 /**
  * Fetches all products based on search query.
- * 
+ *
  * @param query - The search query.
  * @returns {Promise<{ products: Product[] } | ErrorResponse | MessageResponse>} A promise
  */
 export default async function getProductsByQuery(
-    query: string,
+  query: string,
 ): Promise<{ products: Product[] } | MessageResponse | ErrorResponse> {
-    try {
-        const response: AxiosResponse = await axios.get(`${productsURL}`, { params: { search_query: query } });
-        return response.data;
-    } catch (error: unknown) {
-        return errorResponse(error, 'api.products') as MessageResponse | ErrorResponse;
-    }
+  try {
+    const response: AxiosResponse = await axios.get(`${productsURL}`, {
+      params: { search_query: query },
+    });
+    return response.data;
+  } catch (error: unknown) {
+    return errorResponse(error, 'api.products') as MessageResponse | ErrorResponse;
+  }
 }
-
 
 /**
  * Fetches all products based on search query.
- * 
+ *
  * @param query - The search query.
  * @returns {Promise<{ products: Product[] } | ErrorResponse | MessageResponse>} A promise
  */
 export async function getProductsBySearchQuery(
-    query: string,
+  query: string,
 ): Promise<{ products: Product[] } | MessageResponse | ErrorResponse> {
-    try {
-        const response: AxiosResponse = await axios.get(`${productsURL}`, { params: { search_query: query } });
-        return response.data;
-    } catch (error: unknown) {
-        return errorResponse(error, 'api.products') as MessageResponse | ErrorResponse;
-    }
+  try {
+    const response: AxiosResponse = await axios.get(`${productsURL}`, {
+      params: { search_query: query },
+    });
+    return response.data;
+  } catch (error: unknown) {
+    return errorResponse(error, 'api.products') as MessageResponse | ErrorResponse;
+  }
 }
