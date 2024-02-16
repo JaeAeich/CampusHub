c38382a5e0051e917d69081427ec883200a5f3d7
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2024-02-16 12:31:05 +0530

    fix: css and delivery status

--- client/src/components/Orders.tsx
+++ client/src/components/Orders.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { RootState } from '@/store/store';
 import Order from '@/api/orders/types';
 import { getUserOrders } from '@/api/users/users';
+import { getStoreById } from '@/api/stores/stores';
+import Store from '@/api/stores/types';
 import NotFound from './NotFound';
 import Loading from './Loading';
 
@@ -11,6 +13,7 @@ function Orders() {
   const [isLoading, setIsLoading] = useState(true);
   const [orders, setOrders] = useState<Order[]>([]);
   const [error, setError] = useState(false);
+  const [storeImages, setStoreImages] = useState<{ [key: string]: string }>({});
 
   useEffect(() => {
     async function fetchOrders() {
@@ -29,14 +32,33 @@ function Orders() {
     fetchOrders();
   }, [user_id]);
 
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      const promises = orders.map(async (item) => {
+        const response = await getStoreById(item.store_id);
+        if ('store' in response) {
+          const store = response.store as Store;
+          return { [item.order_id]: store.store_images[0] };
+        }
+        return { [item.order_id]: '' };
+      });
+
+      const productDetailsArray = await Promise.all(promises);
+      const details = productDetailsArray.reduce((acc, curr) => ({ ...acc, ...curr }), {});
+      setStoreImages(details as { [key: string]: string });
+    };
+
+    fetchProductDetails();
+  }, [orders]);
+
   if (isLoading) {
     return <Loading />;
   }
 
   if (!orders || error) {
     return (
-      <div className="mx-auto items-center my-auto">
-        <NotFound item="Product" />
+      <div className="flex mx-auto justify-center items-center my-auto">
+        <NotFound item="Orders" />
       </div>
     );
   }
@@ -48,14 +70,19 @@ function Orders() {
           {orders &&
             orders.map((order: Order) => (
               <div className="justify-between mb-6 rounded-lg bg-background p-6 shadow-md sm:flex sm:justify-start">
+                <img
+                  src={storeImages[order.order_id]}
+                  alt="store"
+                  className="w-20 h-20 sm:w-24 sm:h-24 rounded-lg"
+                />
                 <div className="flex flex-col w-full sm:ml-4">
                   <div className="sm:flex sm:w-full sm:justify-between">
                     <div className="mt-5 sm:mt-0">
                       <h2 className="text-lg font-bold text-primary text-subheading">
-                        {order.created_at.slice(0, 10)}
+                        {order.store_name}
                       </h2>
                       <p className="font-semibold md:text-base text-smm text-secondary text-subheading">
-                        {order.store_name}
+                        {order.created_at.slice(0, 10)}
                       </p>
                     </div>
                     <div className="flex flex-col space-between">
