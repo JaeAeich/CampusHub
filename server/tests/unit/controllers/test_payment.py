4b99107999cb351676023bd8609dff8eeba3cea2
Aayush <110016859+aayushkatariaa@users.noreply.github.com>
2024-02-16 14:44:30 +0530

    Update test_payment.py

--- server/tests/unit/controllers/test_payment.py
+++ server/tests/unit/controllers/test_payment.py
@@ -5,36 +5,28 @@ from pydantic import ValidationError
 def test_payment_model():
     # Create a valid payment
     payment_data = {
-        "transaction_id": "123456789",
-        "payment_mode": "UPI",
+        "payment_id": "123456789",
         "amount": 100.0,
-        "description": "Test payment",
-        "source": "Test source",
-        "currency": "USD",
-        "upi_id": "test@upi",
-        "payment_status": "UPI"
+        "order_id": "Test payment",
+        "signature": "Test source",
+        "created_at": "today",
     }
     payment = Payment(**payment_data)
 
-    assert payment.transaction_id == "123456789"
-    assert payment.payment_mode == "UPI"
+    assert payment.payment_id == "123456789"
+    assert payment.created_at == "today"
     assert payment.amount == 100.0
-    assert payment.description == "Test payment"
-    assert payment.source == "Test source"
+    assert payment.order_id == "Test payment"
+    assert payment.signature == "Test source"
     assert payment.currency == "USD"
-    assert payment.upi_id == "test@upi"
-    assert payment.payment_status == "UPI"
 
     # Create a payment with invalid data
     invalid_data = {
-        "transaction_id": "987654321",
-        "payment_mode": "COD",
-        "amount": 50.0,
-        "description": "Invalid payment",
-        "source": "Invalid source",
-        "currency": "XYZ",  #wrong currency
-        "upi_id": "invalid@upi",
-        "payment_status": "COD"
+        "payment_id": "123456789",
+        "amount": "100.0",
+        "order_id": "Test payment",
+        "signature": "Test source",
+        "created_at": "today",
     }
     # Check for a validationerror
     with pytest.raises(ValidationError):
