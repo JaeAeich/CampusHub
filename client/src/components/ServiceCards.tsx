14b263ee1e24e28a8736baf35e42bddf2b53ff01
Aishika Nandi <104081816+aishikanandi@users.noreply.github.com>
2024-04-09 12:33:58 +0530

    community store done (#197)

--- client/src/components/ServiceCards.tsx
+++ client/src/components/ServiceCards.tsx
@@ -30,9 +30,8 @@ export default function ServiceCards({ services, error }: { services: Service[];
                   onClick={() => handleRouteToStore()}
                   className="md:basis-1/2 lg:basis-1/3 sm:basis-1/2"
                 >
-                  <Link to={`/services/${service.service_id}/stores`}>
-                    {/* TODO: Route to the store path onClick */}
-                    <div className="relative group">
+                  {service.service_name !== 'Community Store' ? (
+                    <Link to={`/services/${service.service_id}/stores`} className="relative group">
                       <Card className="bg-gradient-to-r from-secondaryLight to-secondary">
                         <div className="flex-row flex justify-between">
                           <h1 className="px-4 pt-4 xl:text-xl md:text-lg sm:text-md font-subheading font-semibold">
@@ -47,8 +46,25 @@ export default function ServiceCards({ services, error }: { services: Service[];
                           />
                         </CardContent>
                       </Card>
-                    </div>
-                  </Link>
+                    </Link>
+                  ) : (
+                    <Link to="/stores/store3/products" className="relative group">
+                      <Card className="bg-gradient-to-r from-secondaryLight to-secondary">
+                        <div className="flex-row flex justify-between">
+                          <h1 className="px-4 pt-4 xl:text-xl md:text-lg sm:text-md font-subheading font-semibold">
+                            {service.service_name}
+                          </h1>
+                        </div>
+                        <CardContent className="flex sm:aspect-[3/2] aspect-[2] items-center justify-center sm:p-6">
+                          <img
+                            src={service.service_images[0]}
+                            alt={service.service_name}
+                            className="blog-banner h-full transition-transform transform group-hover:scale-110"
+                          />
+                        </CardContent>
+                      </Card>
+                    </Link>
+                  )}
                 </CarouselItem>
               ))}
             </CarouselContent>
